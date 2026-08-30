4b96fba26cdd4f57f5d7a0175f1bdbb768738e2a
michalitsiosp <michalitsios.p@gmail.com>
2026-08-30 13:13:33 +0300

    add json output for netdiscover

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 import os
 import subprocess #for Nmap
 import urllib.request #for api
@@ -8,37 +8,50 @@ from scapy.all import srp, Ether, ARP, conf, IP, sr, ICMP , get_if_addr
 import logging
 logging.getLogger("scapy").setLevel(logging.CRITICAL)
 conf.verb = 0
-print(r"  _   _      _                      _       _____       _  __  __          ")
-print(r" | \ | |    | |                    | |     / ____|     (_)/ _|/ _|         ")
-print(r" |  \| | ___| |___      _____  _ __| | __ | (___  _ __  _| |_| |_ ___ _ __ ")
-print(r" | . ` |/ _ \ __\ \ /\ / / _ \| '__| |/ /  \___ \| '_ \| |  _|  _/ _ \ '__|")
-print(r" | |\  |  __/ |_ \ V  V / (_) | |  |   <   ____) | | | | | | | ||  __/ |   ")
-print(r" |_| \_|\___|\__| \_/\_/ \___/|_|  |_|\_\ |_____/|_| |_|_|_| |_| \___|_|   ")
-print(r"                                                                           ")
+print(r"  _   _       _                 _        _____        _  __  __         ")
+print(r" | \ | |     | |               | |      / ____|      (_)/ _|/ _|         ")
+print(r" |  \| | ___ | |___      _____ | |_| | __ | (___  _ __  _| |_| |_ ___ _ __ ")
+print(r" | . ` |/ _ \ __\ \ /\ / / _ \| '__| |/ /   \___ \| '_ \| |  _|  _/ _ \ '__|")
+print(r" | |\  |  __/ |_ \ V  V / (_) | |  |   <    ____) | | | | | | | ||  __/ |    ")
+print(r" |_| \_|\___|\__| \_/\_/ \___/|_|  |_|\_\  |_____/|_| |_|_|_| |_| \___|_|    ")
+print(r"                                                                             ")
 
-# Ορισμός χρωμάτων με ANSI codes
+# ANSI codes
 RED = "\033[91m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
 CYAN = "\033[96m"
 RESET = "\033[0m"
 
+
+#save_report
+def save(data):
+        try:
+                now = datetime.now()
+                f = now.strftime("%Y-%m-%d_%H-%M-%S")
+                filename = f"report_{f}.json"
+                file = open(filename, "w")
+                file.write(data)
+                file.close()
+                print(f"{GREEN}the report {f} is saved!{RESET}")
+        except Exception as e:
+                print(e)
 #sudo privileges
 def sudo():
-	if os.name == 'posix':
-		#code for Linux/Mac
-		if os.geteuid() != 0:
-			print(f"{RED}[-] Error: Root privileges required{RESET}")
-			sys.exit(1)
-	else:
-		#code for Windows
-		try:
-			import ctypes
-			if not ctypes.windll.shell32.IsUserAnAdmin():
-				print(f"{RED}[-] Error: Administrator  privileges required{RESET}")
-				sys.exit(1)
-		except:
-			pass
+        if os.name == 'posix':
+                #code for Linux/Mac
+                if os.geteuid() != 0:
+                        print(f"{RED}[-] Error: Root privileges required{RESET}")
+                        sys.exit(1)
+        else:
+                #code for Windows
+                try:
+                        import ctypes
+                        if not ctypes.windll.shell32.IsUserAnAdmin():
+                                print(f"{RED}[-] Error: Administrator  privileges required{RESET}")
+                                sys.exit(1)
+                except:
+                        pass
 
 
 
@@ -62,7 +75,7 @@ def traceroute():
     while not ping and ttl <= 30:
         # Στέλνουμε το ICMP πακέτο με το τρέχον TTL
         ans, unans = sr(IP(dst=target, ttl=ttl)/ICMP(), timeout=1, verbose=0)
-        
+
         if ans:
             # Απάντησε κάποιος router ή ο τελικός στόχος
             for sent, received in ans:
@@ -93,27 +106,28 @@ def get_vendor(mac_address):
 
 #netdiscover.py
 def netdiscover():
-	my_ip = get_if_addr(conf.iface)  # returns eg "192.168.55.12"
-	target1 = ".".join(my_ip.split(".")[:-1]) + ".0/24"  
-	try:
-    		broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    		arp_request = ARP(pdst=target1)
-    		packet = broadcast / arp_request
-    		answered, unanswered = srp(packet, timeout=3, verbose=0)
-	except Exception as e:
-    		print(f"[-] Error: {e} ")
-    		sys.exit(1)
-	print("IP Address\t\tMAC Address\t\tDevice")
-	print("-" * 65)
-
-	for sent, received in answered:
-    		vendor = get_vendor(received.hwsrc)
-    		print(f"{received.psrc:<16}\t{received.hwsrc}\t{vendor}")
-    		time.sleep(1)
+        my_ip = get_if_addr(conf.iface)  # returns eg "192.168.55.12"
+        target1 = ".".join(my_ip.split(".")[:-1]) + ".0/24"
+        try:
+                broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+                arp_request = ARP(pdst=target1)
+                packet = broadcast / arp_request
+                answered, unanswered = srp(packet, timeout=3, verbose=0)
+        except Exception as e:
+                print(f"[-] Error: {e} ")
+                sys.exit(1)
+        header = "IP Address\t\tMAC Address\t\tDevice\n" + "-" * 65 + "\n"
+        report_data  = header
+        for sent, received in answered:
+                vendor = get_vendor(received.hwsrc)
+                line = f"{received.psrc:<16}\t{received.hwsrc}\t{vendor}\n"
+                print(line, end="")  # print to screen
+                report_data += line  # add to report
+                time.sleep(1)
+        save(report_data)
 
 def nmap_scan():
     target = input("Enter target IP or Network (e.g., 192.168.1.1): ").strip()
-    
     print("\nSelect Scan Type:")
     print("1. Quick Scan (Fast Top Ports)")
     print("2. Service & Script Scan (Standard -sV -sC)")
@@ -121,7 +135,7 @@ def nmap_scan():
     print("4. Full stealth Scan all ports -sS -p- -Pn --max-rate 100 -T2") 
     ap = input("\nEnter choice (1-4): ").strip()
 
-    # Συγκρίνουμε με strings ("1", "2", "3") και όλα είναι μέσα στη συνάρτηση
+    # nmpap scans
     if ap == "1":
         print(f"\n[*] Starting Quick Scan on {target}...\n")
         args = ["nmap", "-F", target]
@@ -145,32 +159,30 @@ def nmap_scan():
         print("\n[-] Scan cancelled by user.")#selection menu 
 
 def main():
-	sudo()
-	while True:
-		print("**MENU**")
-		print("1. Netdiscover")
-		print("2. Traceroute")
-		print("3. Nmap Scan")
-		print("4. Exit")
-		apanthsh = int(input("Select from (1-4): "))
-		if apanthsh == 1:
-			netdiscover()
-			for i in range(1,4):
+        sudo()
+        while True:
+                print("**MENU**")
+                print("1. Netdiscover")
+                print("2. Traceroute")
+                print("3. Nmap Scan")
+                print("4. Exit")
+                apanthsh = int(input("Select from (1-4): "))
+                if apanthsh == 1:
+                        netdiscover()
+                        for i in range(1,4):
                                 print("-" * 65)
                                 print("-" * 65)
 
-		elif apanthsh == 2:
-			traceroute()
-			for i in range(1,4):
-				print("-" * 65)
-				print("-" * 65)
-		elif apanthsh == 3:
-			nmap_scan()
-		else:
-			print("--Exit--")
-			break
+                elif apanthsh == 2:
+                        traceroute()
+                        for i in range(1,4):
+                                print("-" * 65)
+                                print("-" * 65)
+                elif apanthsh == 3:
+                        nmap_scan()
+                else:
+                        print("--Exit--")
+                        break
 
 if __name__=="__main__":
-	main()
-
-
+        main()
