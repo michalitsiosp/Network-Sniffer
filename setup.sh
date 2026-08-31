94fa40f3ea05ed4a40de82647648e618099d2388
michalitsiosp <michalitsios.p@gmail.com>
2026-08-31 22:20:38 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo apt update
 echo "[*] Installing Nmap and Python dependencies..."
 sudo apt install -y nmap python3-pip
 
+echo "[*] Installing Ping-iputils"
+sudo apt-get install -y iputils-ping
+
 echo "[*] Installing Python packages from requirements.txt..."
 pip3 install -r requirements.txt --break-system-packages 2>/dev/null || pip3 install -r requirements.txt
 
